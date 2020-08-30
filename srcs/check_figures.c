48a8b61fcd54060ab4051b2f8163a39d536453db
jciglesias <jiglesia@student.42.fr>
2020-08-31 00:26:25 +0200

    checking

--- srcs/check_figures.c
+++ srcs/check_figures.c
@@ -6,14 +6,20 @@
 /*   By: jiglesia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/30 18:55:55 by jiglesia          #+#    #+#             */
-/*   Updated: 2020/08/30 18:57:28 by jiglesia         ###   ########.fr       */
+/*   Updated: 2020/08/31 00:25:42 by jiglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
 int	ft_check_sp(char *line)
-{}
+{
+	char	**split;
+
+	split = ft_split(line, ' ');
+	if (!ft_check_xyz(split[1]))
+		return (0);
+}
 
 int	ft_check_pl(char *line)
 {}
